03c4bda6f3fd6974264868406624029a5b1c9a14
CarrotHead1-1 <kieron.escott@icloud.com>
2025-11-21 16:39:14 +0000

    completed bronze layer

--- New Pipeline 2025-11-20 14:24/notebooks/bronze_ingestion.py
+++ New Pipeline 2025-11-20 14:24/notebooks/bronze_ingestion.py
@@ -23,7 +23,7 @@ def clean_column_names(df):
 @dlt.table(
     name = "bronze_dev.default.BoE_Database_Bronze",
     table_properties = {
-        "quality": "landing"
+        "quality": "bronze"
     }
 )
 
@@ -33,4 +33,30 @@ def BoE_Db_raw():
         .option("cloudFiles.format", "csv")
         .load("s3://real-estate-project-area/BoE_Database/")
     )
+    return clean_column_names(df)
+
+@dlt.table(
+    name = "bronze_dev.default.UKHPI_Data_Bronze",
+    table_properties = {"quality": "bronze"}
+)
+
+def UKHPI_Data_raw():
+    df = (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .load("s3://real-estate-project-area/UK_HPI/")
+    )
+    return clean_column_names(df)
+
+@dlt.table(
+    name = "bronze_dev.default.Price_Paid_Data_Bronze",
+    table_properties = {"quality": "bronze"}
+)
+
+def Price_Paid_Data_raw():
+    df = (spark.readStream.format("cloudFiles")
+    .option("cloudFiles.format", "csv")
+    .load("s3://real-estate-project-area/Price_Paid_Data/")
+    )
+    
     return clean_column_names(df)
