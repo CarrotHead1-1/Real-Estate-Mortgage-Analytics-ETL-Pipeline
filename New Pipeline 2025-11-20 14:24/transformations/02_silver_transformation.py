5454445d9404fb23c37aeb2627da124e5133fa1c
CarrotHead1-1 <kieron.escott@icloud.com>
2025-12-01 22:58:52 +0000

    tested ingest, fix date issues

--- New Pipeline 2025-11-20 14:24/transformations/02_silver_transformation.py
+++ New Pipeline 2025-11-20 14:24/transformations/02_silver_transformation.py
@@ -8,7 +8,7 @@ from pyspark.sql.functions import to_date, col, current_timestamp, when, count, 
 
 #create expectations and validation
 @dlt.expect("valid_date", "Date IS NOT NULL")
-@dlt.expect("valid_data", "Monthly_number_of_total_sterling_approvals_for_house_purchase_to_individuals_seasonally_adjusted_______________a______________LPMVTVX IS NOT NULL")
+@dlt.expect("valid_data", "Monthly_Approvals IS NOT NULL")
 
 
 def BoE_Database_Silver():
@@ -16,7 +16,7 @@ def BoE_Database_Silver():
 
     #rename columns, cast values
     df_clean = (
-        df.withColumn("Date", to_date("Date", "dd-MM-yyyy"))
+        df.withColumn("Date", to_date("Date", "dd MMM yy"))
         .withColumnRenamed("Monthly_number_of_total_sterling_approvals_for_house_purchase_to_individuals_seasonally_adjusted_______________a______________LPMVTVX","Monthly_Approvals")
         .withColumn("Monthly_Approvals", col("Monthly_Approvals").cast("int"))
         .select("Date", "Monthly_Approvals", "_ingest_file")
@@ -65,7 +65,7 @@ def UKHPI_Data_Silver():
     df = df.dropDuplicates(["Date", "RegionName"])
     
     #convert data column and cast numerical values to double
-    df = df.withColumn("Date", to_date("Date", "dd-MM-yyyy"))
+    df = df.withColumn("Date", to_date("Date", "dd/MM/yyyy"))
     
     columns_numeric = [
         "AveragePrice","Index","IndexSA","1m_Change","12m_Change","AveragePriceSA","SalesVolume","DetachedPrice","DetachedIndex","Detached1m_Change","Detached12m_Change","SemiDetachedPrice","SemiDetachedIndex",	"SemiDetached1m_Change","SemiDetached12m_Change","TerracedPrice","TerracedIndex","Terraced1m_Change","Terraced12m_Change","FlatPrice","FlatIndex","Flat1m_Change","Flat12m_Change","CashPrice","CashIndex","Cash1m_Change",	"Cash12m_Change","CashSalesVolume","MortgagePrice","MortgageIndex","Mortgage1m_Change","Mortgage12m_Change","MortgageSalesVolume","FTBPrice","FTBIndex","FTB1m_Change","FTB12m_Change","FOOPrice","FOOIndex","FOO1m_Change","FOO12m_Change","NewPrice","NewIndex","New1m_Change","New12m_Change","NewSalesVolume","OldPrice","OldIndex", "Old1m_Change","Old12m_Change","OldSalesVolume"
@@ -113,7 +113,7 @@ def Price_Paid_Data_Silver():
     df_cleaned = (
         df.withColumn("TransactionId", regexp_replace(col("TransactionId"), "[{}]", ""))
         .withColumn("Price", col("Price").cast("int"))
-        .withColumn("DateOfTransaction", to_date("DateOfTransaction", "dd-MM-yyyy"))
+        .withColumn("DateOfTransaction", to_date("DateOfTransaction", "yyyy-MM-dd"))
         .withColumn("Postcode", trim(col("Postcode")))
         .withColumn("PAON", trim(col("PAON")))
         .withColumn("SAON", trim(col("SAON")))
--- New Pipeline 2025-11-20 14:24/transformations/03_gold_layer.sql
+++ New Pipeline 2025-11-20 14:24/transformations/03_gold_layer.sql
@@ -1,30 +1,62 @@
 
-CREATE OR REFRESH STREAMING LIVE TABLE gold_dev.default.DimDate
-COMMENT "Date Dimension"
-AS
-SELECT 
-    CAST(date_col AS DATE) AS Date,
-    DATE_FORMAT(date_col, "ddMMyyyy") as DateKey,
-    YEAR(date_col) as YEAR,
-    MONTH(date_col) as MONTH,
-    DATE_FORMAT(date_col, "MMM") as MONTHNAME,
-    DAY(date_col) as DAY
-FROM (
-    SELECT DISTINCT Date FROM silver_dev.default.UKHPI_Data_Silver
-    UNION 
-    SELECT DISTINCT DateOfTransaction FROM silver_dev.default.Price_Paid_Data_Silver
-    UNION 
-    SELECT DISTINCT Date FROM silver_dev.default.BoE_Database_Silver
-) d(date_col);
+-- CREATE OR REFRESH STREAMING LIVE TABLE gold_dev.default.DimDate
+-- COMMENT "Date Dimension"
+-- AS
+-- SELECT 
+--     CAST(date_col AS DATE) AS Date,
+--     DATE_FORMAT(date_col, "ddMMyyyy") as DateKey,
+--     YEAR(date_col) as YEAR,
+--     MONTH(date_col) as MONTH,
+--     DATE_FORMAT(date_col, "MMM") as MONTHNAME,
+--     DAY(date_col) as DAY
+-- FROM (
+--     SELECT DISTINCT Date FROM silver_dev.default.UKHPI_Data_Silver
+--     UNION 
+--     SELECT DISTINCT DateOfTransaction FROM silver_dev.default.Price_Paid_Data_Silver
+--     UNION 
+--     SELECT DISTINCT Date FROM silver_dev.default.BoE_Database_Silver
+-- ) d(date_col);
 
-CREATE OR REFRESH LIVE TABLE gold_dev.default.DimRegion (
-  RegionId BIGINT GENERATED ALWAYS AS IDENTITY,
-  RegionName STRING,
-  AREACODE String
-)
-AS SELECT
-  DISTINCT RegionName,
-  AreaCode
-FROM silver_dev.default.UKHPI_Data_Silver
-WHERE RegionName IS NOT NULL AND AreaCode IS NOT NULL;
+-- CREATE OR REFRESH LIVE TABLE gold_dev.default.DimRegion (
+--   RegionId BIGINT GENERATED ALWAYS AS IDENTITY,
+--   RegionName STRING,
+--   AREACODE String
+-- )
+-- AS SELECT
+--   DISTINCT RegionName,
+--   AreaCode
+-- FROM silver_dev.default.UKHPI_Data_Silver
+-- WHERE RegionName IS NOT NULL AND AreaCode IS NOT NULL;
+
+-- CREATE OR REFRESH LIVE TABLE gold_dev.default.DimLocation (
+--   LocationId BIGINT GENERATED ALWAYS AS IDENTITY,
+--   Postcode STRING,
+--   PAON STRING,
+--   SAON STRING,
+--   District STRING,
+--   TownCity STRING,
+--   County STRING
+-- )
+-- AS SELECT 
+--   DISTINCT Postcode,
+--   PAON,
+--   SAON,
+--   District,
+--   TownCity,
+--   County
+-- FROM silver_dev.default.Price_Paid_Data_Silver
+-- WHERE Postcode IS NOT NULL AND TownCity IS NOT NULL;
+
+-- CREATE OR REFRESH LIVE TABLE gold_dev.default.DimPropertyType (
+--   PropertyTypeId BIGINT GENERATED ALWAYS AS IDENTITY,
+--   PropertyCategory STRING,
+--   IsNew BOOLEAN,
+--   Duration STRING
+-- )
+-- AS SELECT
+--   DISTINCT PropertyType as PropertyCategory,
+--   OldNew as IsNew, 
+--   Duration
+-- FROM silver_dev.default.Price_Paid_Data_Silver
+-- WHERE PropertyType IS NOT NULL AND OldNew IS NOT NULL AND Duration IS NOT NULL
 
